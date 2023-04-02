863941494c06832c842501703c08e4a80a100add
muranakar <p1211065@gmail.com>
2023-04-03 07:10:27 +0900

    Initial Commit

---
+++ JapaneseZodiacGame/GoogleAdmob/GoogleAdID.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleAdID.swift
+//  ChildDevelopmentSupport
+//
+//  Created by 村中令 on 2022/05/30.
+//
+
+import Foundation
+
+struct GoogleAdID {
+    static let appID = ""
+    static let startBannerID = "ca-app-pub-3713368437594912/9711474188"
+    static let gameBannerID = "ca-app-pub-3713368437594912/6752416151"
+    static let resultBannerID = "ca-app-pub-3713368437594912/2163306597"
+    static let gameScreenInterstitialID = "ca-app-pub-3713368437594912/7085310847"
+    static let startScreenRewardAdID = ""
+}
